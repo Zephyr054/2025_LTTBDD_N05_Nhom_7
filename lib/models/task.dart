0fd1b0903e1680d22b4878fa77a954aed6ab04cf
Zephyr054 <tandunnguyen5347@gmail.com>
2025-11-09 18:36:39 +0700

    Update task.dart

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,13 +1,32 @@
 class Task {
+  final String id;
   String title;
   String description;
   String status; // Chưa làm / Đang làm / Hoàn thành
   DateTime deadline;
 
   Task({
+    required this.id,
     required this.title,
     required this.description,
     required this.status,
     required this.deadline,
   });
+  // Chuyển đối tượng Task -> Map (để lưu JSON)
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'description': description,
+    'status': status,
+    'deadline': deadline.toIso8601String(),
+  };
+
+  // Chuyển Map -> Task (để đọc lại)
+  factory Task.fromJson(Map<String, dynamic> json) => Task(
+    id: json['id'],
+    title: json['title'],
+    description: json['description'],
+    status: json['status'],
+    deadline: DateTime.parse(json['deadline']),
+  );
 }
